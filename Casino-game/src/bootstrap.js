9d4e71be6e910afd792dc2048a43da29e799abfb
Ballred95 <blake.allred95@icloud.com>
2020-06-06 00:04:03 -0600

    built general starting idea of number generator for slots, still working on code to work right

--- Casino-game/src/bootstrap.js
+++ Casino-game/src/bootstrap.js
@@ -1,8 +1,20 @@
-class SlotMachine{
-    constructor()
-}
+// class SlotMachine{
+//     constructor()
+// }
 
 
 function randomNumberGenerator(){
+    let container = []
+    for(let i=0; i>5; i++){
+       let num = Math.floor(Math.random() * 10)
+       container.push(num)
+    }
+     
+    return container;
     
 }
+
+console.log(randomNumberGenerator())
+
+
+
