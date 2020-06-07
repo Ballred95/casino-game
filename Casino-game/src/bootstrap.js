62a840a70b7bede1d58eee9fff61f7cfe1bf8078
Ballred95 <blake.allred95@icloud.com>
2020-06-06 19:35:51 -0600

    assigned this attributes to slotmachine class to create different slot arguments and updated readme.

--- Casino-game/src/bootstrap.js
+++ Casino-game/src/bootstrap.js
@@ -1,6 +1,15 @@
-// class SlotMachine{
-//     constructor()
-// }
+class SlotMachine{
+    constructor(slot1, slot2, slot3, slot4, slot5){
+        this.slot1=slot1
+        this.slot2=slot2
+        this.slot3=slot3
+        this.slot4=slot4
+        this.slot5=slot5
+    }
+    
+}
+
+
 const _ = require('lodash');
 
 function randomNumberGenerator(){
@@ -9,10 +18,9 @@ function randomNumberGenerator(){
     return container
     }
      
-    
-    
-
-
     console.log(randomNumberGenerator())
+    let disneySlots = new SlotMachine('hm', 'space', 'TOT', 'matterhorn', 'thunder')
+    console.log(disneySlots)
+    console.log(disneySlots.slot4)
 
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 This will be a caisno type game created in javascript
 The game will be in the style of a slot machine where you have to get 5 in a row of a certain type to win money.
+
+Slot-mahcine Theme: DISNEY RIDES
