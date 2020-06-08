dfff34543d5656f1939150b3e51ae14f39d95365
Ballred95 <blake.allred95@icloud.com>
2020-06-07 20:30:59 -0600

    added slotAssignment feature

--- Casino-game/src/bootstrap.js
+++ Casino-game/src/bootstrap.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 class SlotMachine{
     constructor(slot1, slot2, slot3, slot4, slot5){
         this.slot1=slot1
@@ -5,22 +7,22 @@ class SlotMachine{
         this.slot3=slot3
         this.slot4=slot4
         this.slot5=slot5
+        
     }
+    
     
 }
 
 
-const _ = require('lodash');
-
 function randomNumberGenerator(){
     let container = []
-    let numArray =_.times(5, ()=>container.push(_.random(0,10)))
+    let numArray =_.times(5, ()=>container.push(_.random(0,5)))
     return container
-    }
-     
-    console.log(randomNumberGenerator())
-    let disneySlots = new SlotMachine('hm', 'space', 'TOT', 'matterhorn', 'thunder')
-    console.log(disneySlots)
-    console.log(disneySlots.slot4)
+    
+}
 
+let slotAssignment = new SlotMachine(_.random(0,5), _.random(0,5), _.random(0,5), _.random(0,5), _.random(0,5))
+console.log(slotAssignment)
+    
+    
 
